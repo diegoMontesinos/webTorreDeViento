ae4c681dedacf8ca407925692088d6c0d9dfcd15
diegoMontesinos <chesp.gommont@gmail.com>
2014-02-22 10:41:21 -0600

    Avance Validaciones. Diego

--- app/controllers/colaborators_controller.rb
+++ app/controllers/colaborators_controller.rb
@@ -98,63 +98,12 @@ class ColaboratorsController < ApplicationController
 
   # PATCH
   def update
+    permitted_params = colaborator_params # Parametros filtrados (permitidos)
+
     @colaborator = Colaborator.find(params[:id])
-
-    if @colaborator.frequent
-      
-      if params[:previous].present?
-        @colaborator.previous = params[:previous].to_i
-      else
-        @colaborator.previous = @colaborator.id
-      end
-
-      if params[:next].present?
-        @colaborator.next = params[:next].to_i
-      else
-        @colaborator.next = @colaborator.id
-      end
-      
-    else
-      colaborators = Colaborator.colaborators_inorder
-
-      colaborators.each.with_index { |colab, index|
-        if colab.is_a? Colaborator
-          if colab.id == @colaborator.id
-
-            if index - 1 >= 0
-              if colaborators[index - 1].is_a? Colaborator
-                @colaborator.previous = colaborators[index - 1].id
-              else
-                @colaborator.previous = "-1"
-              end
-            else
-              if colaborators[colaborators.length - 1].is_a? Colaborator
-                @colaborator.previous = colaborators[colaborators.length - 1].id
-              else
-                @colaborator.previous = "-1"
-              end
-            end
-
-            if index + 1 < colaborators.length
-              if colaborators[index + 1].is_a? Colaborator
-                @colaborator.next = colaborators[index + 1].id
-              else
-                @colaborator.next = "-1"
-              end
-            else
-              if colaborators[0].is_a? Colaborator
-                @colaborator.next = colaborators[0].id
-              else
-                @colaborator.next = "-1"
-              end
-            end
-
-            break
-          end
-        end
-      }
-
-    end
+    @colaborator.update(permitted_params)
+    
+    redirect_to @colaborator
   end
 
   # DELETE
